f2a13e7122df2085ba397342ffc21f55f77dedd4
dimasah901 <93038732+dimasah901@users.noreply.github.com>
2025-11-28 21:06:57 +0500

    Autocreate a table if not exists

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ class Car(QWidget):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         self.connection = Connection("coffee.sqlite")
+        self.connection.autocommit = True
+        self.connection.cursor().execute("""
+            CREATE TABLE IF NOT EXISTS coffee (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                sort TEXT,
+                rarity TEXT,
+                state TEXT,
+                taste TEXT,
+                price TEXT,
+                volume TEXT
+            )
+        """).fetchall()
         self.data = self.connection.cursor().execute("SELECT * FROM coffee").fetchall()
         self.ui.tableWidget.setRowCount(len(self.data))
         for i, row in enumerate(self.data):
