2c9c5b889fb1a0d79e3f9012504ac686b5682e30
dimasah901 <dimasah901@gmail.com>
2025-11-28 20:38:19 +0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import (QWidget, QApplication, QLabel, QTableWidgetItem)
+from main_ui import Ui_Form
+from sqlite3 import Connection
+
+class Car(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.connection = Connection("coffee.sqlite")
+        self.data = self.connection.cursor().execute("SELECT * FROM coffee").fetchall()
+        self.ui.tableWidget.setRowCount(len(self.data))
+        for i, row in enumerate(self.data):
+            for j, item in enumerate(row):
+                self.ui.tableWidget.setItem(i, j, QTableWidgetItem(item))
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Car()
+    ex.show()
+    sys.exit(app.exec())
